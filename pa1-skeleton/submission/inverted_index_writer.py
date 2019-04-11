35d125a38895b6e8fecf87b9913a8c58ea544343
Yi Zhong <yizhon@microsoft.com>
2019-04-10 23:04:18 -0700

    add InvertedIndexWriter

---
+++ pa1-skeleton/submission/inverted_index_writer.py
@@ -0,0 +1,40 @@
+class InvertedIndexWriter(InvertedIndex):
+    """"""
+    def __enter__(self):
+        self.index_file = open(self.index_file_path, 'wb+')              
+        return self
+
+    def append(self, term, postings_list):
+        """Appends the term and postings_list to end of the index file.
+        
+        This function does three things, 
+        1. Encodes the postings_list using self.postings_encoding
+        2. Stores metadata (postings_begin_position, 
+                            number_of_postings_in_list,
+                            length_of_postings_list_in_bytes)
+        3. Appends the bytestream to the index file on disk
+
+        Hint: You might find it helpful to read the Python I/O docs
+        (https://docs.python.org/3/tutorial/inputoutput.html) for
+        information about appending to the end of a file.
+        
+        Parameters
+        ----------
+        term:
+            term or termID is the unique identifier for the term
+        postings_list: List[Int]
+            List of docIDs where the term appears
+        """
+        ### Begin your code
+        #step 1
+        encoded_posting_list = self.postings_encoding.encode(postings_list)
+        
+        #step 2
+        postings_begin_position = postings_list[0]
+        number_of_postings_in_list = len(postings_list)
+        length_of_postings_list_in_bytes =  len(encoded_posting_list)
+        
+        #step3
+        self.postings_dict[term] = (postings_begin_position,number_of_postings_in_list,length_of_postings_list_in_bytes)  
+        print(self.postings_dict[term])
+        ### End your code
---
+++ pa1-skeleton/submission/parse_block.py
@@ -0,0 +1,27 @@
+class BSBIIndex(BSBIIndex):            
+    def parse_block(self, block_dir_relative):
+        """Parses a tokenized text file into termID-docID pairs
+        
+        Parameters
+        ----------
+        block_dir_relative : str
+            Relative Path to the directory that contains the files for the block
+        
+        Returns
+        -------
+        List[Tuple[Int, Int]]
+            Returns all the td_pairs extracted from the block
+        
+        Should use self.term_id_map and self.doc_id_map to get termIDs and docIDs.
+        These persist across calls to parse_block
+        """
+        ### Begin your code
+        td_pairs = []
+        for filename in os.listdir(self.data_dir +'/'+ block_dir_relative):
+            with open(self.data_dir +'/'+ block_dir_relative +'/'+ filename, 'r') as f:
+                doc_id = self.doc_id_map.__getitem__(filename)
+                for s in f.read().split():
+                    term_id = self.term_id_map.__getitem__(s)
+                    td_pairs.append((term_id, doc_id))
+        print(td_pairs)
+        ### End your code
